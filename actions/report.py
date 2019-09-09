d4cc5a3c1da7c19bd63a42622fa1fbb682806a87
Michael Drew Palazola <mpalazola@hotmail.com>
2019-09-09 15:21:52 -0500

    got the mapping to work with biomes and the report

--- actions/report.py
+++ actions/report.py
@@ -1,5 +1,11 @@
 def build_facility_report(arboretum):
     for river in arboretum.rivers:
         print(f'River [{river.id}]')
+        for animal in river.animals:
+            print(animal.species, animal.id)
+    for coastline in arboretum.coastlines:
+        print(f'Coastline [{coastline.id}] [{coastline.animals}]')
+
+
 
     input("\n\nPress any key to continue...")
