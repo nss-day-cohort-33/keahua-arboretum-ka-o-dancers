b39ca4a6354df81aea58d3676144577ba074bb9e
Jeff Hill <jeffhill1978@gmail.com>
2019-09-11 12:02:14 -0500

    added plants to report

--- plants/silversword.py
+++ plants/silversword.py
@@ -1,8 +1,9 @@
 from plants.plant import Plant
+from interfaces import Identifiable
 
-class Silversword(Plant):
+class Silversword(Plant, Identifiable):
 
     def __init__(self):
         Plant.__init__(self, "Silversword")
-
+        Identifiable.__init__(self)
 
